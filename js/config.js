fa8ed6a8671f265f737fd92ed2123aec26569dba
QUARTER-salon <y.funabashi@quarter.salon>
2025-03-14 21:25:22 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window.CONFIG = {
+  APPS_SCRIPT_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxA-xuRc_F0Ih1KE9r9YXfOJ5WJqF0vUZvm3Eb_aQ9coqBjJzoA3nNoRuxNmajK06Xceg/exec',
+  STORE_REVIEW_URLS: {
+    'QUARTER': 'https://g.page/r/CfiWzYV0WLCdEBE/review',
+    'QUARTER RESORT': 'https://g.page/r/CUpu9_cAhdaGEBE/review',
+    'QUARTER SEASONS': 'https://g.page/r/CWAu_dLl0DJmEBE/review',
+    'LINK': 'https://g.page/r/CYLblbqgWXsREBE/review',
+    'iL': 'https://g.page/r/CemPjkInZSpLEBE/review'
+  }
+};
